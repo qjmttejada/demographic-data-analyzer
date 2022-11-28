6106217faf2b17f6c9744436394c4997711e5f53
Michael Tejada <76642717+qjmttejada@users.noreply.github.com>
2022-11-28 09:55:02 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from demographic_data_analyzer import calculate_demographic_data
+from unittest import main
+
+# Test your function by calling it here
+calculate_demographic_data()
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
